e39655548d56fb1172f838561f6ac92b018cb653
Risto Kuusterä <risto.kuustera@gofore.com>
2019-11-03 20:19:46 +0200

    Update instructions and use configured topic name

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ Adapted from
 [here](https://github.com/simplesteph/kafka-stack-docker-compose)
 and 
 [here](https://github.com/wkorando/event-stream-kafka)
+
+## Usage
+
+Start Kafka stack: `docker-compose up`
+
+From `kafka-tester`-directory start the app: `mvn spring-boot:run`
+
+Messages can be sent at `http://localhost:44444/send/{message}`
+
+Messages can be read at `http://localhost:44444/messages`
--- kafka-tester/src/main/java/org/alkukampela/kafkatester/KafkaConfig.java
+++ kafka-tester/src/main/java/org/alkukampela/kafkatester/KafkaConfig.java
@@ -1,14 +1,18 @@
 package org.alkukampela.kafkatester;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KafkaConfig {
 
+    @Value("${listener.topic}")
+    private String topicName;
+
     @Bean
     public NewTopic createTopic() {
-        return new NewTopic("test", 1, (short) 1);
+        return new NewTopic(topicName, 1, (short) 1);
     }
 }
